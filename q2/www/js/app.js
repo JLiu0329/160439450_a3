9463e2902e7522438223473c88157d9c0ddcd3af
Junyi Liu <junyi.z.liu@gmail.com>
2019-11-11 02:08:20 -0500

    3rd commit

---
+++ q2/www/js/app.js
@@ -0,0 +1,81 @@
+function validateEmail() {
+  var email = document.getElementById("email").value;
+
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  var test = re.test(String(email).toLowerCase());
+
+  if (!test) {
+    alert('Invalid email.')
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateEmailNoRegex() {
+  var email = document.getElementById("email").value;
+
+  if (email == '') {
+    alert("Empty email.");
+    document.getElementById("email").focus();
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateUsername() {
+  var username = document.getElementById("username").value;
+  var re = [^a-zA-Z0-9];
+
+  var test = re.test(username);
+
+  if (!test) {
+    alert("Invalid username.");
+
+    return false;
+  }
+
+  return true;
+}
+
+function validatePassword() {
+  var password = document.getElementById("password").value;
+
+  if (password == '') {
+    alert("Empty password.");
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateForm() {
+  if (!validateEmail()) {
+    return false;
+  }
+
+  if (!validateUsername()) {
+    return false;
+  }
+
+  if (!validatePassword()) {
+    return false;
+  }
+
+  return true;
+}
+
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
